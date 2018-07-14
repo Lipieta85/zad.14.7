2c2ef5b11ce2d260ada6a11384d8844a768f34d5
Rafał Lipieta <cwgflojdid@wp.pl>
2018-07-14 10:41:49 +0200

    Change ContactForm.js

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -6,20 +6,23 @@ var ContactForm = React.createClass({
   render: function () {
     return (
       <form className={'contactForm'}>
-        <div className={'form-group'}>
-        
+
+        <div className={'form-group'}>        
           <input className={'form-control'} type={'text'} placeholder={'Imię'} value={this.props.contact.Name} />
         </div>
-        <div class="form-group">
+
+        <div className={'form-group'}>
           <input className={'form-control'} type={'text'} placeholder={'lastName'} value={this.props.contact.lastName} />
         </div>
-        <div class="form-group">
-        
+
+        <div className={'form-group'}>       
           <input className={'form-control'} type={'email'} placeholder={'Email'} value={this.props.contact.email} />
         </div>
+
         <p>
           <button className={'btn btn-success'} type={'submit'}>Dodaj kontakt</button>
         </p>
+
       </form>
     )
   },
